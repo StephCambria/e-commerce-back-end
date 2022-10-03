ff674e2133869314ab93e2f35eeb6af3d6886bc8
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-10-03 14:38:44 -0400

    sequelize won't read mysql data

--- config/connection.js
+++ config/connection.js
@@ -5,13 +5,20 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
       host: 'localhost',
+      port: 3306,
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
     });
+
+sequelize.authenticate(() => {
+  console.log('Connection successful!');
+}).catch((err) => {
+  if (err) throw err;
+})
 
 module.exports = sequelize;
 
